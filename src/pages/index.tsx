36e3e5970be8f83533c75b4ca9b78b866484d1ce
Lance Samonte <lancesamonte@awesomelabph.com>
2023-04-26 01:32:11 +0800

    remove google font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
 import { fetchEventSource } from "@microsoft/fetch-event-source";
 import styles from "@/styles/Home.module.css";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { FormEvent, useCallback, useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const router = useRouter();
